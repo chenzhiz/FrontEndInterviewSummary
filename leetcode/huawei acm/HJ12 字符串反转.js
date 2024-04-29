e91fe9f0fc7213196e2d476e237861b4ca6137bb
Chen Zhiz <295272231@qq.com>
2024-04-29 21:59:48 +0800

    feat: huawei acm HJ12 字符串反转

---
+++ leetcode/huawei acm/HJ12 字符串反转.js
@@ -0,0 +1,15 @@
+const rl = require("readline").createInterface({ input: process.stdin });
+var iter = rl[Symbol.asyncIterator]();
+const readline = async () => (await iter.next()).value;
+
+void async function () {
+    // Write your code here
+    while(line = await readline()){
+        let arr = [...line]
+        let result = ""
+        for(let i = arr.length -1; i>=0; i--){
+            result += arr[i]
+        }
+        console.log(result)
+    }
+}()
